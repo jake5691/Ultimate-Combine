62d86f4ab4dc37bb9dfd4bd9b29bf9058f0e979a
jake5691 <25382582+jake5691@users.noreply.github.com>
2026-01-26 12:33:54 +0100

    pagetitle Startseite

--- Webspace/index.php
+++ Webspace/index.php
@@ -108,7 +108,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Ultimate Combine – Login</title>
+  <title>Ultimate Combine</title>
   <link rel="icon" href="assets/favicon.ico">
   <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png">
   <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png">
